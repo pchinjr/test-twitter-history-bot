622048fe7e6ee873ba9b22260e3781efbda59129
Paul Chin Jr <pchinjr@gmail.com>
2021-03-05 06:38:51 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,57 +1,42 @@
-exports.handler = async function http(req) {
-
-  let html = `
-<!doctype html>
-<html lang=en>
-  <head>
-    <meta charset=utf-8>
-    <title>Hi!</title>
-    <link rel="stylesheet" href="https://static.begin.app/starter/default.css">
-    <link href="data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=" rel="icon" type="image/x-icon">
-  </head>
-  <body>
-
-    <h1 class="center-text">
-      <!-- ↓ Change "Hello world!" to something else and head on back to Begin! -->
-      Hello world!
-    </h1>
-
-    <p class="center-text">
-      Your <a href="https://begin.com" class="link" target="_blank">Begin</a> app is ready to go!
-    </p>
-
-  </body>
-</html>`
-
-  return {
-    headers: {
-      'content-type': 'text/html; charset=utf8',
-      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
-    },
-    statusCode: 200,
-    body: html
-  }
-}
-
-// Other example responses
-
-/* Forward requester to a new path
-exports.handler = async function http (req) {
-  return {
-    statusCode: 302,
-    headers: {'location': '/about'}
-  }
-}
-*/
-
-/* Respond with successful resource creation, CORS enabled
 let arc = require('@architect/functions')
-exports.handler = arc.http.async (http)
-async function http (req) {
+const { TwitterClient } = require('twitter-api-client')
+const axios = require('axios')
+
+async function route(req) {
+
+  const twitterClient = new TwitterClient({
+    apiKey: process.env.TWITTER_API_KEY,
+    apiSecret: process.env.TWITTER_API_SECRET,
+    accessToken: process.env.TWITTER_ACCESS_TOKEN,
+    accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  })
+
+  await axios.get('http://history.muffinlabs.com/date')
+    .then(response => {
+      const data = response.data.data ? response.data.data : {}
+      let tweet
+      if (data.Events && data.Events.length) {
+        //tweet the first event in the array
+        tweet = 'Year ' + data.Events[0].year + ' - ' + data.Events[0].text
+      } else {
+        tweet = 'Nothing happened today :)'
+      }
+      
+      twitterClient.tweets.statusesUpdate({
+        status: tweet
+      }).then(response => {
+        console.log("Tweeted!", response)
+      }).catch(err => {
+        console.error(err)
+      })
+    }).catch(err => {
+      console.error(err)
+    })
+
   return {
-    statusCode: 201,
-    json: { ok: true },
-    cors: true,
+    statusCode: 200,
+    html: 'praise cage'
   }
 }
-*/
+
+exports.handler = arc.http.async(route)
